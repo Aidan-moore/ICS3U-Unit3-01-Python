f9d8b5dcbca79305b2aed4e9d2a650444e999405
Ubuntu <ubuntu@ip-172-31-39-191.ec2.internal>
2021-09-29 17:07:11 +0000

    commit message

---
+++ simlpe_adding.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Aidan Moore
+# Created on: Sept 2021
+# This program shows how to do simple addition
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
